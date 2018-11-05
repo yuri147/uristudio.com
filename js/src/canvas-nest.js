29fb182f2b9928bfe572bc73320b03cc8b44b31e
余力 <li1.yu@BKJK.COM>
2018-11-05 16:17:25 +0800

    Site updated: 2018-11-05 16:17:23

--- js/src/canvas-nest.js
+++ js/src/canvas-nest.js
@@ -143,8 +143,24 @@ window.requestAnimationFrame =
   window.webkitRequestAnimationFrame ||
   window.msRequestAnimationFrame;
 
+function setupCanvas(canvas) {
+  // Get the device pixel ratio, falling back to 1.
+  var dpr = window.devicePixelRatio || 1;
+  // Get the size of the canvas in CSS pixels.
+  var rect = canvas.getBoundingClientRect();
+  // Give the canvas pixel dimensions of their CSS
+  // size * the device pixel ratio.
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+  var ctx = canvas.getContext('2d');
+  // Scale all drawing operations by the dpr, so you
+  // don't have to worry about the difference.
+  ctx.scale(dpr, dpr);
+  return ctx;
+}
+
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+var ctx = setupCanvas(canvas);
 var w = (canvas.width = canvas.offsetWidth);
 var h = (canvas.height = canvas.offsetHeight);
 var circles = [];
